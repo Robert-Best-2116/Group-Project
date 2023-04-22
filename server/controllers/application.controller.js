784a10a206e010fb55615222f9480918dad72f8c
KatieInnes <katharine.innes@gmail.com>
2023-04-21 21:10:23 -0700

    updated controller with error codes

--- server/controllers/application.controller.js
+++ server/controllers/application.controller.js
@@ -1,21 +1,31 @@
 const Application = require('../models/application.model')
 
+
+// FIND ALL
 module.exports.findAllApplications = (request, response) => {
     Application.find().sort({_id:-1})
         .then(allApplications => {
-            response.json(allApplications)
+            response.status(200).json(allApplications)
         })
-        .catch(err => response.json(err));
+        .catch((err) => {
+            res.status(500).json({err})
+        });
 }
 
+
+// FIND ONE
 module.exports.findOneApplication = (request, response) => {
     Application.findOne({ _id: request.params.id})
         .then(oneApplication => {
             response.json(oneApplication)
         })
-        .catch(err => response.json(err));
+        .catch((err) => {
+            res.status(400).json({err})
+        });
 }
 
+
+// UPDATE
 module.exports.updateApplicationById= (request, response) => {
     Application.findOneAndUpdate(
         { _id: request.params.id },
@@ -25,9 +35,13 @@ module.exports.updateApplicationById= (request, response) => {
         .then(updateApplication => {
             response.json(updateApplication)
         })
-        .catch(err => response.json(err));
+        .catch((err) => {
+            res.status(400).json({err})
+        });
 }
 
+
+// CREATE
 module.exports.createNewApplication = (request, response) => {
     Application.create(request.body) 
         .then(application => {
@@ -38,10 +52,14 @@ module.exports.createNewApplication = (request, response) => {
         })
 }
 
+
+// DELETE
 module.exports.deleteAnExistingApplication = (request, response) => {
     Application.deleteOne({ _id: request.params.id })
         .then(deleteConfirmation => {
             response.json(deleteConfirmation)
         })
-        .catch(err => response.json(err));
+        .catch((err) => {
+            response.status(400).json({err})
+        })
 }
