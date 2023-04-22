ddf7ac0c2266f7263837d4254d3eb19dc507aac0
Alex Konnaris <alexkonnaris125@gmail.com>
2023-04-22 18:50:08 -0400

    Adding Create and Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-
 // import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import ViewPost from './components/ViewPost';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import ViewPost from "./components/ViewPost";
+import { useState } from "react";
+import CreatePost from "./components/CreatePost";
 
 function App() {
+  // STATE VALUE TO HOLD ALL APPLICATIONS - Alex
+  // WILL BE USED BY CreatePost AND Dashboard - Alex
+  const [applications, setApplications] = useState([]);
+
   return (
     <Router>
-    <div className="App">
-
-      <Routes>
-
-        <Route path='/dashboard' element={<Dashboard />} />
-        {/* Will need to implement ID in Route and on Button in card in Dashboard page*/}
-        <Route path='/view' element={<ViewPost />} />
-
-      </Routes>
-     
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* Will need to implement ID in Route and on Button in card in Dashboard page*/}
+          <Route path="/view" element={<ViewPost />} />
+          {/* CREATE APPLICATION PAGE BELOW - Alex */}
+          <Route
+            path="/create"
+            element={
+              <CreatePost
+                applications={applications}
+                setApplications={setApplications}
+              />
+            }
+          />
+        </Routes>
+      </div>
     </Router>
   );
 }
