f26fd735b0cf448fa5861ac9c88c8f244ec6ea08
KatieInnes <katharine.innes@gmail.com>
2023-04-25 13:43:51 -0700

    Added text to the about page. Have bios in for Katie and Alex, need those from Robert, Brittany, and Jordan.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,7 +2,60 @@
 const About = () => {
     return (
         <div>
-            ABOUT PAGE
+            <div>
+                <h2>Application Tracker was created as the group project component for the final class of Coding Dojo's full stack engineering program. The creators are:</h2> {/*Jordan, please make this look pretty. I'm adding some basic styling just for my own sake. -Katie */}
+            </div>
+
+            <div>
+                {/* image */}
+                <h4>Robert Best, Project Lead</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, commodi. Ullam quasi modi est asperiores eligendi qui voluptate expedita accusamus eveniet libero minima nulla, rem aspernatur officiis illum vel facilis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem omnis harum optio pariatur, molestias neque quidem libero magnam voluptatibus veniam nam nostrum, unde ad aut tenetur laborum dolor, exercitationem eius. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <br></br>
+                <p> Labore quos assumenda voluptas possimus id autem reiciendis culpa saepe, quaerat dignissimos. Voluptates error, veritatis perspiciatis soluta natus quidem tenetur iste ipsa!</p>
+                <a href="https://www.linkedin.com/in/robert-a-best/">LinkedIn</a>
+                <a href="https://github.com/Robert-Best-2116">Github</a>
+            </div>
+
+            <div>
+                {/* image */}
+                <h4>Jordan Graham, CSS/Styling</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, commodi. Ullam quasi modi est asperiores eligendi qui voluptate expedita accusamus eveniet libero minima nulla, rem aspernatur officiis illum vel facilis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem omnis harum optio pariatur, molestias neque quidem libero magnam voluptatibus veniam nam nostrum, unde ad aut tenetur laborum dolor, exercitationem eius. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <br></br>
+                <p> Labore quos assumenda voluptas possimus id autem reiciendis culpa saepe, quaerat dignissimos. Voluptates error, veritatis perspiciatis soluta natus quidem tenetur iste ipsa!</p>
+                <a href="https://www.linkedin.com/">LinkedIn</a> 
+                <a href="https://github.com/">Github</a>
+            </div>
+            
+            <div>
+                {/* image */}
+                <h4>Katie Innes, Back-End Development</h4>
+                <p>Using Robert's detailed wireframe, I kicked off the project, creating the server side of the application. After writing the code for the config, controller, model, and route files I tested it all in Postman to ensure the CRUD functions worked and to add some sample entries to the Mongo database.</p>
+                <br></br>
+                <p>I've worked in a variety of industries—theatre, elementary education, non-profit and for-profit marketing, and health tech—but it is that last one that sparked my interest in learning to code. I got a taste of QA work, helping to debug my company's websites on the front end, but now I'd like to use my full-stack engineering skills to become a QA engineer.</p>
+                <a href="https://www.linkedin.com/in/katharineinnes/">LinkedIn</a> 
+                <a href="https://www.github.com/katieinnes">Github</a>
+            </div>
+
+            <div>
+                {/* image */}
+                <h4>Alex Konnaris, Front-End Development</h4>
+                <p>Based on the wireframe that Robert designed, I created a create and edit form on the front end of our project using state values and axios requests. These forms allowed us to add and edit applications in our database. I also displayed validations from the backend which were made by Katie and I made part of the form render conditionally because some of the inputs are optional.</p>
+                <br></br>
+                <p>I'm from NYC born and raised and I've worked in the warehouse/shipping industry for 2+ years. I discovered my passion for programming during the pandemic and I'm working towards a career in fintech because I believe I can learn more about managing money and making good investments. I hope to open a business one day but I'm also open to working in many different fields of tech that way I can meet lots of people and learn new things.</p>
+                <a href="https://www.linkedin.com/in/alexander-konnaris-6044a1220/">LinkedIn</a> 
+                <a href="https://github.com/alexkonnaris">Github</a>
+            </div>
+
+            <div>
+                {/* image */}
+                <h4>Brittany Lystrup, Login and Registration</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, commodi. Ullam quasi modi est asperiores eligendi qui voluptate expedita accusamus eveniet libero minima nulla, rem aspernatur officiis illum vel facilis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem omnis harum optio pariatur, molestias neque quidem libero magnam voluptatibus veniam nam nostrum, unde ad aut tenetur laborum dolor, exercitationem eius. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <br></br>
+                <p> Labore quos assumenda voluptas possimus id autem reiciendis culpa saepe, quaerat dignissimos. Voluptates error, veritatis perspiciatis soluta natus quidem tenetur iste ipsa!</p>
+                <a href="https://www.linkedin.com/in/brittany-lystrup/">LinkedIn</a> 
+                <a href="https://github.com/BLystrup">Github</a>
+            </div>
+
         </div>
     )
 }
